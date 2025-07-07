feff4bdabd87fa4898f8e8edada2ed5ac5731d83
Dastan <dastiw1910@gmail.com>
2025-07-07 19:14:32 +0600

    Add views, content

--- apps/tours/urls.py
+++ apps/tours/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.tours.views import hello_view, goodbye_view
+from apps.tours.views import hello_view, goodbye_view, category_list
 
 urlpatterns = [
-    path('python/', hello_view),
-    path('js/', goodbye_view),
+    path('index/', hello_view, name='index_view'),
+    path('elements/', goodbye_view, name='elements_view'),
+    path('generic/', category_list, name='category_list'),
 ]
--- apps/tours/views.py
+++ apps/tours/views.py
@@ -1,10 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from apps.tours.models import PlaceCategory, Place
+
 
 def hello_view(request):
     return render(request, 'index.html')
 
 
 def goodbye_view(request):
-    return render(request, 'generic.html')
+    return render(request, 'elements.html')
+
+
+def category_list(request):
+    categories = PlaceCategory.objects.all()
+    places = Place.objects.all()
+
+
+    return render(request, 'generic.html', {'categories': categories, 'places': places})
